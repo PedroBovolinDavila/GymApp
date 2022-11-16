c0e176fc1b4fabdf101370e1744c5542f45ba030
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-11-16 17:29:40 -0300

    feat: added function for select image from gallery

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
-import { TouchableOpacity } from "react-native";
-import { Center, ScrollView, VStack, Skeleton, Text, Heading } from "native-base";
+import { TouchableOpacity, Alert } from "react-native";
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from "native-base";
+
+import * as ImagePicker from 'expo-image-picker'
+import * as FileSystem from 'expo-file-system'
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
@@ -9,6 +12,51 @@ import { Button } from "@components/Button";
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
+  const [userPhoto, setUserPhoto] = useState('https://avatars.githubusercontent.com/u/118199084?v=4')
+
+  const toast = useToast()
+
+  async function handleSelectUserPhoto() {
+    try {
+      setPhotoIsLoading(true)
+
+      const selectedPhoto = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+        
+      })
+  
+      if (selectedPhoto.cancelled) {
+        return 
+      }
+
+      if (selectedPhoto.uri) {
+        const photoInfo = await FileSystem.getInfoAsync(selectedPhoto.uri)
+        
+        if (!photoInfo.size) {
+          return
+        }
+
+        const photoSizeInMegabytes = photoInfo.size / 1024 / 1024
+        
+        if (photoSizeInMegabytes > 2) {
+          return toast.show({
+            title: 'Essa imagem é muito grande! Escolha uma menor que 5 megas.',
+            placement: 'top',
+            bg: 'red.500',
+          })
+        }
+
+        setUserPhoto(selectedPhoto.uri)
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setPhotoIsLoading(false)
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -27,13 +75,13 @@ export function Profile() {
               />
             ) : (
               <UserPhoto
-                source={{ uri: 'https://avatars.githubusercontent.com/u/118199084?v=4' }}
+                source={{ uri: userPhoto }}
                 alt="Foto do usuario"
                 size={33}
               />
             )
           }
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleSelectUserPhoto}>
             <Text
               color="green.500"
               fontWeight="bold"
